fac2e4814d6345e14d1ba52cb96f3f860928eda4
Annabelle <109839853+annabelleaaron@users.noreply.github.com>
2023-10-02 20:51:20 +1300

    setup
    bun, elysia, typed-html

---
+++ index.tsx
@@ -0,0 +1,33 @@
+import {Elysia} from "elysia";
+import {html} from "@elysiajs/html";
+import * as elements from "typed-html";
+
+const app = new Elysia()
+    .use(html())
+    .get("/", ({html}) => 
+        html(
+        <BaseHtml>
+            <body>
+                <button hx-post="/clicked" hx-swap="outerHTML">
+                    Click Me
+                </button>
+            </body>
+        </BaseHtml>
+        )
+    )
+    .post("/clicked", () => <div>I'm from the server!</div>)
+    .listen(3000);
+console.log(`Elysia is running at http://${app.server?.hostname}:${app.server?.port}`);
+
+const BaseHtml = ({children}: elements.Children) => `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>THE BETH STACK</title>
+</head>
+
+${children}
+`;
